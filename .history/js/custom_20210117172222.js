75a9decf87d056edd4606a97f81259a5a97a06da
zhadjah9559 <zhadjah@gmail.com>
2021-01-17 17:24:26 -0500

    styling index.html

---
+++ .history/js/custom_20210117172222.js
@@ -0,0 +1,24 @@
+document.getElementById("userInputBtn").addEventListener("click", ()=>{
+    //userInput = {"input"}
+    let userInput = document.getElementById("userInput").value
+
+    //convert string to an array  { "i", "n", "p", "u", "t" }
+    let arrayElements = userInput.split('')
+
+    //reverse array  =  { "t", "u", "p", "n", "i"}
+    let reverseArray = arrayElements.reverse();
+
+    //reversestring  =  {"tupni"}
+    let reversedString = reverseArray.join('');
+
+    if(userInput == reversedString){
+        document.getElementById("output").innerHTML = "🌮The input you entered is a Palindrome!!😻"
+    }else{
+        document.getElementById("output").innerHTML = "❌The input you entered is NOT a Palindrome😿"
+    }    
+    userInput.value = ""
+})
+
+function clear(){
+    document.getElementById("userInputBtn").reset(); 
+}
